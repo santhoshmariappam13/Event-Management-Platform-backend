abcdaf3c1f9edb499abc074b13e00594f2fdaaa1
santhosh <santhoshmariappan13@gmail.com>
2025-03-01 22:01:20 +0530

    Update paymentRoutes.js

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const paymentController = require('../controllers/paymentController'); // Importing the controller
+const paymentController = require('../controllers/paymentController'); 
 
-// Debugging log to ensure the controller has been imported correctly
-console.log(paymentController); // Check if functions are being imported
+console.log(paymentController); 
 
-// Ensure the post routes are correctly defined
 router.post('/create-payment-intent', paymentController.createPaymentIntent);
 router.post('/webhook', express.raw({ type: 'application/json' }), paymentController.webhook);
 
