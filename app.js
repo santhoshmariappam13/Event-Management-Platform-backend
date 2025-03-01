90555b10fcda21c3dad6dac7bb9b7456f0d3555d
santhosh <santhoshmariappan13@gmail.com>
2025-03-01 21:56:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,25 +3,23 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 
-// Import Routes
+
 const authRoutes = require('./src/routes/authRoutes');
 const eventRoutes = require('./src/routes/eventRoutes');
 const adminRoutes = require('./src/routes/adminRoutes');
-const paymentRoutes = require('./src/routes/paymentRoutes'); // Import payment routes
-
+const paymentRoutes = require('./src/routes/paymentRoutes'); 
 dotenv.config();
 
 const app = express();
 
-// Middleware
-app.use(cors());
-app.use(express.json()); // Parse JSON requests
-app.use(bodyParser.json()); // Parse incoming JSON requests
 
-// Routes
+app.use(cors());
+app.use(express.json()); 
+app.use(bodyParser.json()); 
+
+
 app.use('/api/auth', authRoutes);
 app.use('/api/events', eventRoutes);
 app.use('/api/admin', adminRoutes);
-app.use('/api/payment', paymentRoutes); // Define payment-related routes
-
+app.use('/api/payment', paymentRoutes);
 module.exports = app;
